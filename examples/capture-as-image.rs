bfcc823abb7446f37338f24664c6135ad5883d03
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2023-07-26 20:02:09 +0900

    依存ライブラリを更新した。

--- examples/capture-as-image.rs
+++ examples/capture-as-image.rs
@@ -14,15 +14,15 @@ fn main() -> Result<(), Error> {
                 .value_name("FILE")
                 .help("Specify output filename")
                 .required(true)
-                .conflicts_with("list")
-                .takes_value(true),
+                .conflicts_with("list"),
         )
         .arg(
             Arg::new("fullscreen")
                 .short('f')
                 .long("fullscreen")
                 .help("Specify if full screen capture taking")
-                .conflicts_with_all(&["window", "list"]),
+                .conflicts_with_all(&["window", "list"])
+                .num_args(0),
         )
         .arg(
             Arg::new("window")
@@ -30,36 +30,37 @@ fn main() -> Result<(), Error> {
                 .long("window")
                 .value_name("TITLE")
                 .help("Specify target window title")
-                .conflicts_with_all(&["fullscreen", "list"])
-                .takes_value(true),
+                .conflicts_with_all(&["fullscreen", "list"]),
         )
         .arg(
             Arg::new("desktop")
                 .short('d')
                 .long("desktop")
                 .help("Specify if desktop window taking")
-                .conflicts_with_all(&["fullscreen", "list"]),
+                .conflicts_with_all(&["fullscreen", "list"])
+                .num_args(0),
         )
         .arg(
             Arg::new("list")
                 .short('l')
                 .long("list")
                 .help("List desktop window name")
-                .conflicts_with_all(&["output", "fullscreen", "window"]),
+                .conflicts_with_all(&["output", "fullscreen", "window"])
+                .num_args(0),
         )
         .get_matches();
 
-    if matches.is_present("list") {
+    if matches.get_flag("list") {
         print_window_name()?;
         return Ok(());
     }
 
-    let output = matches.value_of("output").unwrap();
-    let fullscreen = matches.is_present("fullscreen");
-    let window = matches.value_of("window");
-    let is_desktop = matches.is_present("desktop");
+    let output = matches.get_one::<String>("output").unwrap();
+    let fullscreen = matches.get_flag("fullscreen");
+    let window = matches.get_one::<String>("window");
+    let is_desktop = matches.get_flag("desktop");
 
-    let bmp = capture_as_image(fullscreen, window, is_desktop)?;
+    let bmp = capture_as_image(fullscreen, window.map(|x| x.as_str()), is_desktop)?;
 
     let format = image::ImageFormat::from_path(output).unwrap();
     let img = image::load_from_memory_with_format(&bmp, image::ImageFormat::Bmp).unwrap();
